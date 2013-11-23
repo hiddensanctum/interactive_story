ee041de18c1061af9c327f8a6da6378c720f4868
James Chuang <jameschuang13@gmail.com>
2013-11-23 12:17:56 -0800

    add views for books and add basic creation of books with pages on same page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,18 +3,25 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
-  def show
-    @books = Book.new
-
+  def new
+    @book = Book.new
+    1.times do
+      page = @book.pages.build
+    end
   end
 
-  def new
+  def show
+    @book = Book.find(params[:id])
   end
 
   def edit
   end
 
   def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to @book
+    end
   end
 
   def update
@@ -22,4 +29,9 @@ class BooksController < ApplicationController
 
   def destroy
   end
+
+  private
+    def book_params
+      params.require(:book).permit(:name, :author, :level, :num_page, :description, questions_attributes: [:id, :image, :story, :book_id, '_destroy'])
+    end
 end
