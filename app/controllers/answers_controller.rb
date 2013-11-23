ec5a7925867916fcda8415ac5f9f782cbb57fcd8
Thanh Huynh <thanhjamin@yahoo.com>
2013-11-23 12:41:39 -0800

    update answer and solution controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,61 @@
 class AnswersController < ApplicationController
+  before_action :set_answer, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @answers = Answer.all
+  end
+
+  def show
+  end
+
+  def new
+    @answer = Answer.new
+  end
+
+  def edit
+  end
+
+  def create
+    @answer = Answer.new(answer_params)
+
+    respond_to do |format|
+      if @answer.save
+        format.html { redirect_to @answer, notice: 'Answer was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @answer }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @answer.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @answer.update(answer_params)
+        format.html { redirect_to @answer, notice: 'Answer was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @answer.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @answer.destroy
+    respond_to do |format|
+      format.html { redirect_to answers_url }
+      format.json { head :no_content }
+    end
+  end
+
+private
+
+  def set_answer
+    @answer = Answer.find(params[:id])
+  end
+
+  def answer_params
+      params.require(:answer)
+  end
 end
--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -1,2 +1,39 @@
 class SolutionsController < ApplicationController
+  def index
+    @solutions = Solutions.all
+  end
+
+  def show
+    @solution = Solutions.find(params[:id])
+  end
+
+  def new
+    @solution = Solutions.new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def create
+    @solution = Solutions.new(solution_params)
+
+    respond_to do |format|
+      if @solution.save
+        format.html { redirect_to @solution, notice: 'Question was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @solution }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @solutions.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def solution_params
+    params.require(:solution)
+  end
 end
