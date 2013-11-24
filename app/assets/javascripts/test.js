b4bd7516353ebb7b1d8cad776c22a101341ca723
James Chuang <jameschuang13@gmail.com>
2013-11-24 13:04:24 -0800

    fix javascript and add assets

--- app/assets/javascripts/test.js
+++ app/assets/javascripts/test.js
@@ -1,27 +1,19 @@
 var ready;
 ready = function () {
-  document.getElementById("wrong_1").onclick = function(){
-    alert('WRONG!');
-  }
-  document.getElementById("wrong_2").onclick = function(){
-    alert('WRONG!');
-  }
-  document.getElementById("wrong_3").onclick = function(){
-    alert('WRONG!');
-  }
-  document.getElementById("wrong_4").onclick = function(){
-    alert('WRONG!');
-  }
-  document.getElementById("wrong_5").onclick = function(){
-    alert('WRONG!');
-  }
-  document.getElementById("wrong_6").onclick = function(){
-    alert('WRONG!');
-  }
   document.getElementById("right").onclick = function(){
     alert('Correct!');
     window.location.href = document.URL.slice(0, document.URL.indexOf("?")+1) + 'page=' + (parseInt(document.URL.slice(document.URL.indexOf("=")+1))+1) ;
   }
+}
+function rightAnswer(){
+  alert('Correct!');
+  if (document.URL.indexOf("?") > -1 ){
+    window.location.href = document.URL.slice(0, document.URL.indexOf("?")+1) + 'page=' + (parseInt(document.URL.slice(document.URL.indexOf("=")+1))+1) ;
+  }
+}
+
+function wrongAnswer(){
+  alert('WRONG!');
 }
 
  $(document).ready(ready)
