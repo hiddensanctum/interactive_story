63dacfec1e7a99f27af81299ba63656dae3f4108
James Chuang <jameschuang13@gmail.com>
2013-11-24 10:22:15 -0800

    implement quiz functionality and remove unneeded assets

---
+++ app/assets/javascripts/test.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+  document.getElementById("wrong").onclick = function(){
+    alert('WRONG!');
+  }
+  document.getElementById("right").onclick = function(){
+    alert('Correct!');
+  }
+}
